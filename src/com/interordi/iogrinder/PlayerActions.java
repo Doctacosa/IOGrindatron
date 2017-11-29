df2214faae833c70a2f3705c5d2ee8f6177acd75
Stéphane Lemay <interordi@interordi.com>
2017-11-29 03:53:51 +0000

    + major penalty on death

--- src/com/interordi/iogrinder/PlayerActions.java
+++ src/com/interordi/iogrinder/PlayerActions.java
@@ -1,8 +1,11 @@
 package com.interordi.iogrinder;
 
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 public class PlayerActions implements Listener {
 
@@ -17,4 +20,14 @@ public class PlayerActions implements Listener {
 	}
 	
 
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent event) {
+		@SuppressWarnings("unused")
+		EntityType entity = event.getEntityType();
+		
+		if (event.getEntity() instanceof Player) {
+			Player p = (Player)event.getEntity();
+			Players.getPlayerWatcher(p).subEnergy(25);
+		}
+	}
 }
